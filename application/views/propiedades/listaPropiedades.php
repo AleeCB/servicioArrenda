5c09a7a463368c3c915bb61ad5242f3d4aa6128d
Almita05 <martinezlanderoalmalil@gmail.com>
2022-11-04 15:14:27 -0600

    Primeros cambios

--- application/views/propiedades/listaPropiedades.php
+++ application/views/propiedades/listaPropiedades.php
@@ -1,28 +1,41 @@
 <div class="container">
-	<table class="table table-striped table-border">
-		<thead> 
-			<th>Nombre</th>
-			<th>Ubicación</th>
-			<th>Número de habitantes</th>
-			<th>Precio</th>
-			<th>Estatus</th>
-			<th>Amueblado</th>
-			<th>Tipo propiedad</th>
-			<th>Foto</th>
-		</thead>
-		<tbody>
-			<tr>
-				 <!-- ciclo para renderizar los datos  -->
-				 <?php foreach ($propiedades as $key): ?>
-				<!--  	fila por cada producto -->
 
-				 	<tr>
-				 		<!-- imprimir la variable key, haciendo refrencia al atributo de la tabla  -->
-				 		<td> <?=$key->idPropiedad ?></td>
-				 	</tr>
-				 <?php endforeach ?>
-			</tr>
-		</tbody>
+    <a class="btn btn-success" href="<?=base_url('index.php/PropiedadesC/insertarPropiedad/')?>">AGREGAR
+        PROPIEDAD</a>
 
-	</table>
+    <table class="table table-striped table-bordered">
+        <thead class="table-success">
+            <th class="text-center">Id</th>
+            <th class="text-center">Nombre de la propiedad</th>
+            <th class="text-center">Ubicación</th>
+            <th class="text-center">Número de habitantes</th>
+            <th class="text-center">Precio</th>
+            <th class="text-center">Estatus</th>
+            <th class="text-center">Amueblado</th>
+            <th class="text-center">Tipo de propiedad</th>
+            <th class="text-center">Foto</th>
+            <th class="text-center">ACCION</th>
+        <tbody class="table-primary">
+            <?php foreach($propiedades as $key): ?>
+            <tr>
+                <td><?=$key->idPropiedad?></td>
+                <td><?=$key->nombre?></td>
+                <td><?=$key->ubicacion?></td>
+                <td><?=$key->numHabitantes?></td>
+                <td><?=$key->precio?></td>
+                <td><?=$key->estatus?></td>
+                <td><?=$key->amueblado?></td>
+                <td><?=$key->tipoPropiedad?></td>
+                <td><?=$key->fotoPrincipal?></td>
+                <td><a class="btn btn-success btn-sm"
+                        href="<?=base_url('index.php/PropiedadesC/editarPropiedad/').$key->idPropiedad?>">EDITAR</a>
+                    <a class="btn btn-danger btn-sm"
+                        href="<?=base_url('index.php/PropiedadesC/eliminarPropiedad/').$key->idPropiedad?>">ELIMINAR</a>
+                </td>
+
+            </tr>
+            <?php endforeach ?>
+        </tbody>
+        </thead>
+    </table>
 </div>
