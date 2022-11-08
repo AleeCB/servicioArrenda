71cff14971d194552733f2a3653da6e83716f9a0
Almita05 <martinezlanderoalmalilia@gmail.com>
2022-11-08 15:11:22 -0600

    gffg

--- application/views/headers/menu.php
+++ application/views/headers/menu.php
@@ -1,36 +1,37 @@
 <nav class="navbar" style="background-color: #FFFFFF;">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
+
+            <button class="btn btn-info" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+                aria-controls="offcanvasExample">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample"
+                aria-labelledby="offcanvasExampleLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menú</h5>
+                    <button type="button" class="btn-close" aria-label="Close"></button>
+                </div>
+                <div></div>
+                <div class="dropdown mt-3">
+                    <button class="btn btn-success" type="button">
+                        Perfil
+                    </button>
+                </div>
+                <div class="dropdown mt-3">
+                    <button class="btn btn-success" type="button">
+                        Mis propiedades
+                    </button>
+                </div>
+                <div class="dropdown mt-3">
+                    <button class="btn btn-success" type="button">
+                        Reservas
+                    </button>
+                </div>
+            </div>
             <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/281/7826038281_05bdbfb5-ba7b-4213-9e02-3488afd0c93c.png?cb=1667763509"
                 width="220" height="40">
-        </a>
-        <button class="btn btn-info" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
-            aria-controls="offcanvasExample">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample"
-            aria-labelledby="offcanvasExampleLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menú</h5>
-                <button type="button" class="btn-close" aria-label="Close"></button>
-            </div>
-            <div></div>
-            <div class="dropdown mt-3">
-                <button class="btn btn-success" type="button">
-                    Perfil
-                </button>
-            </div>
-            <div class="dropdown mt-3">
-                <button class="btn btn-success" type="button">
-                    Mis propiedades
-                </button>
-            </div>
-            <div class="dropdown mt-3">
-                <button class="btn btn-success" type="button">
-                    Reservas
-                </button>
-            </div>
-        </div>
+
         </a>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -44,4 +45,3 @@
     </div>
     //VAMO A VER QUE SHOW
 </nav>
-
