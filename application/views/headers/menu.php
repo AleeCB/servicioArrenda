dac22ffdbe4a3368c5a69a1ab0bfbedb4f6ee614
AleeCB <alexcb1028@gmail.com>
2022-11-08 15:02:22 -0600

    xd

--- application/views/headers/menu.php
+++ application/views/headers/menu.php
@@ -1,8 +1,6 @@
 <nav class="navbar" style="background-color: #FFFFFF;">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
-        <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/281/7826038281_05bdbfb5-ba7b-4213-9e02-3488afd0c93c.png?cb=1667763509"  width="220" height="40">
-    </a>
             <button class="btn btn-info" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
                 aria-controls="offcanvasExample">
                 <span class="navbar-toggler-icon"></span>
@@ -30,8 +28,8 @@
                     </button>
                 </div>
             </div>
-            <img src="https://o.remove.bg/downloads/ba702c25-c656-407b-a718-32199db39fe1/image-removebg-preview.png"
-                alt="LogoFS" width="280" height="60" class="d-inline-block align-text-top">
+            <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/281/7826038281_05bdbfb5-ba7b-4213-9e02-3488afd0c93c.png?cb=1667763509"  width="220" height="40">
+    
         </a>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
