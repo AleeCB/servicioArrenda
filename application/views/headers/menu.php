67806f0c31bdd401f00c4305bd412b2901ded042
Almita05 <martinezlanderoalmalilia@gmail.com>
2022-11-10 13:32:23 -0600

    Holiiiiiii

--- application/views/headers/menu.php
+++ application/views/headers/menu.php
@@ -1,30 +1,30 @@
-<nav class="navbar" style="background-color: #e3f2fd;">
+<nav class="navbar">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
-            <button class="btn btn-info" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+            <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
                 aria-controls="offcanvasExample">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <img src="https://o.remove.bg/downloads/c023399f-73f7-485f-876f-f69a51018faa/image-removebg-preview.png"
-                alt="LogoFS" width="280" height="60" class="d-inline-block align-text-top">
+            <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/281/7826038281_05bdbfb5-ba7b-4213-9e02-3488afd0c93c.png?cb=1667763509"
+                width="220" height="40" class="d-inline-block align-text-top">
             <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample"
-                aria-labelledby="offcanvasExampleLabel">
-                <div class="offcanvas-header">
+                aria-labelledby="offcanvasExampleLabel" style="color: white; background-color: #103263;">
+                <div class="offcanvas-header" style="color: white; background-color: #31AFB4;">
                     <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menú</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="dropdown mt-3">
-                    <a href="index.php/PropiedadesC/perfil" type="button" class="btn btn-success">
+                    <a href="index.php/PropiedadesC/perfil" type="button" class="btn btn-lg" style="color: white;">
                         Perfil
                     </a>
                 </div>
-                <div class="dropdown mt-3">
-                    <a href="index.php/PropiedadesC/perfil" type="button" class="btn btn-success">
+                <div class=" dropdown mt-1">
+                    <a href="index.php/PropiedadesC/perfil" type="button" class="btn btn-lg" style="color: white;">
                         Mis propiedades
                     </a>
                 </div>
-                <div class="dropdown mt-3">
-                    <a href="index.php/PropiedadesC/perfil" type="button" class="btn btn-success">
+                <div class="dropdown mt-1">
+                    <a href="index.php/PropiedadesC/perfil" type="button" class="btn btn-lg" style="color: white;">
                         Reservas
                     </a>
                 </div>
