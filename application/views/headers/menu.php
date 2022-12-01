c3242a09f4248ff62381b8ad98dc68e8a65de575
Almita05 <martinezlanderoalmalilia@gmail.com>
2022-11-30 23:35:25 -0600

    Cambios

--- application/views/headers/menu.php
+++ application/views/headers/menu.php
@@ -8,7 +8,7 @@
             <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/281/7826038281_05bdbfb5-ba7b-4213-9e02-3488afd0c93c.png?cb=1667763509"
                 width="220" height="40" class="d-inline-block align-text-top">
             <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample"
-                aria-labelledby="offcanvasExampleLabel" style="color: white; background-color: #103263;">
+                aria-labelledby="offcanvasExampleLabel" style="color: white; background-color: #155772;">
                 <div class="offcanvas-header" style="color: white; background-color: #31AFB4;">
                     <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menú</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -30,22 +30,18 @@
                 </div>
             </div>
         </a>
-        <div>
-            <!-- Botón para lo del perfil -->
-            <div class="btn-group">
-                <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    Pedro
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Perfil</a></li>
-                    <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <!-- Botón para lo del perfil -->
+        <button type="button" class="btn outline-dark" class="d-grid gap-2 d-md-block col-6 mx-auto"
+            data-bs-toggle="modal" data-bs-target="#iniciar"><svg xmlns="http://www.w3.org/2000/svg" width="35"
+                height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                <title>Iniciar sesión</title>
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                <path fill-rule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+            </svg>
+        </button>
+
+    </div>
     </div>
 </nav>
 
